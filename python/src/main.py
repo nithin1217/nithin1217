51b98cc108117f19c66223d2b61a0bb48583ba0d
nithin1217 <108689346+nithin1217@users.noreply.github.com>
2022-07-06 13:24:11 +0530

    updated py

--- python/src/main.py
+++ python/src/main.py
@@ -1,21 +1,20 @@
+from logging import exception
+from shutil import move
+from warnings import catch_warnings
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import speech_recognition as sr
 import time
 import pyttsx3
-ser = Service("C:\\Users\\navinithin\\Downloads\\chromedriver.exe")
-op = webdriver.ChromeOptions()
-s = webdriver.Chrome(service=ser, options=op)
+from gtts import gTTS
+import os
+from lxml import html
+import requests
+from bs4 import BeautifulSoup
+import re
 
-s.maximize_window()
-
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-
-recognizer = sr.Recognizer()
-microphone = sr.Microphone()
 
 def speak(query):
     engine.say(query)
@@ -31,64 +30,71 @@ def recognize_speech():
     except:
         response = "Error"
     return response
-time.sleep(3)
-speak("Hello master! I am now online..")
-while True:
-    speak("How can I help you?")
-    voice = recognize_speech().lower()
-    print(voice)
-    if 'open google' in voice:
-        speak('Opening google..')
-        s.execute_script("window.open('');")
-        window_list = s.window_handles
-        s.switch_to_window(window_list[-1])
-        s.get('https://google.com')
-    elif 'search google' in voice:
-        while True:
-            speak('I am listening..')
-            query = recognize_speech()
-            if query != 'Error':
-                break
-        element = s.find_element_by_name('q')
-        element.clear()
-        element.send_keys(query)
-        element.send_keys(Keys.RETURN)
-    elif 'open youtube' in voice:
-        speak('Opening youtube..')
-        s.execute_script("window.open('');")
-        window_list = s.window_handles
-        s.switch_to_window(window_list[-1])
-        s.get('https://youtube.com')
-    elif 'search youtube' in voice:
-        while True:
-            speak('I am listening..')
-            query = recognize_speech()
-            if query != 'Error':
-                break
-        element = s.find_element_by_name('search_query')
-        element.clear()
-        element.send_keys(query)
-        element.send_keys(Keys.RETURN)
-    elif 'switch tab' in voice:
-        num_tabs = len(s.window_handles)
-        cur_tab = 0
-        for i in range(num_tabs):
-            if s.window_handles[i] == s.current_window_handle:
-                if i != num_tabs - 1:
-                    cur_tab = i + 1
-                    break
-        s.switch_to_window(s.window_handles[cur_tab])
-    elif 'close tab' in voice:
-        speak('Closing Tab..')
-        s.close()
-    elif 'go back' in voice:
-        s.back()
-    elif 'go forward' in voice:
-        s.forward()
-    elif 'exit' in voice:
-        speak('Goodbye Master!')
-        s.quit()
-        break
-    else:
-        speak('Not a valid command. Please try again.')
-    time.sleep(2)
+
+def extractQuestion(con):
+    reg_str = '<p>(.*)</p>'
+    res = re.findall(reg_str, con)
+    return str(res[0])
+
+def extractOptions(con):
+    reg_str = '(?<=<span>)([\w|\d|\n|\s]*)(?=<\/span>)'
+    res = re.findall(reg_str, con)
+    return res  
+
+def extractSection(con):
+    reg_str = '<section>(\s.*\s.*\s.*)</section>'
+    return re.findall(reg_str, con)
+
+  
+
+
+def speakQuestion():
+    src = driver.page_source
+    section = extractSection(src)[0]
+    options = extractOptions(section)
+    ques =  extractQuestion(section)
+
+    speak(ques)
+    speak('Options are')
+
+    for i in options:
+        speak(str(i))
+
+
+def onStart():
+    element=driver.find_element(By.XPATH,'//*[@id="1"]')
+    element.click() 
+    element=driver.find_element(By.XPATH,'/html/body/div[2]/div/button[2]')
+    element.click()
+
+
+# ser = Service("C:\\Users\\navinithin\\Downloads\\chromedriver.exe")
+# op = webdriver.ChromeOptions()
+# driver = webdriver.Chrome(service=ser, options=op)
+
+driver = webdriver.Chrome("D:\Quiz Application with Timer\python\driver\chromedriver.exe")
+url= "https://nithin1217.github.io"
+driver.get(url)
+driver.maximize_window()
+# url="file:///D:/Quiz%20Application%20with%20Timer/index.html"
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+language ='en'
+recognizer = sr.Recognizer()
+microphone = sr.Microphone()
+recognizer.energy_threshold=10000
+time.sleep(1)
+speak("Hi! I am now online..")
+onStart()
+
+speakQuestion()
+# while 1:
+    # speak("How can I help you?")
+    # voice = recognize_speech().lower()
+    # if('start' in voice):
+    #     onStart()
+    #     while True:
+    #         cmove()
+
+ 
